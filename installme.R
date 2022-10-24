ad507439e72a281e73e9b9d0396898645bd636d2
mgreen <mgreen@bournemouth.ac.uk>
2022-10-24 14:07:47 +0100

    make renv do beginr

--- installme.R
+++ installme.R
@@ -2,5 +2,5 @@
 # in the place on the C drive that IT let us write to on BU devices
 
 .libPaths(new=c("C:/ProgramData/R/Rlibs/R420"))
-install.packages(c("patchwork", "readr", "magrittr", "ggplot2", "readr", "dplyr", "tidyr", "patchwork", "devtools", "markdown"), lib="C:/ProgramData/R/Rlibs/R420")
-devtools::install_github("mjgreen/beginr", lib="C:/ProgramData/R/Rlibs/R420")
+install.packages(c("patchwork", "readr", "magrittr", "ggplot2", "readr", "dplyr", "tidyr", "patchwork", "devtools", "markdown"))
+renv::install("mjgreen/beginr")

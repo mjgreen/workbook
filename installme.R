11dd005d46855cad7546f166e85e816042785101
mgreen <mgreen@bournemouth.ac.uk>
2022-10-24 13:57:39 +0100

    simplifying for use in P101

--- installme.R
+++ installme.R
@@ -2,17 +2,5 @@
 # in the place on the C drive that IT let us write to on BU devices
 
 .libPaths(new=c("C:/ProgramData/R/Rlibs/R420"))
-
-if (!require("tidyverse")){install.packages("tidyverse", lib="C:/ProgramData/R/Rlibs/R420", type="binary")}
-
-if (!require("patchwork")){install.packages("patchwork", lib="C:/ProgramData/R/Rlibs/R420", type="binary")}
-
-if (!require("Hmisc")){install.packages("Hmisc", lib="C:/ProgramData/R/Rlibs/R420", type="binary")}
-
-if (!require("pander")){install.packages("pander", lib="C:/ProgramData/R/Rlibs/R420", type="binary")}
-
-if (!require("devtools")){install.packages("devtools", lib="C:/ProgramData/R/Rlibs/R420", type="binary")}
-
-#if (!require("beginr")){install.packages("beginr_0.1.2.zip", lib="C:/ProgramData/R/Rlibs/R420", type="binary", repos=NULL)}
-
-if (!require("beginr")){devtools::install_github("mjgreen/beginr", lib="C:/ProgramData/R/Rlibs/R420")}
+install.packages(c("patchwork", "readr", "magrittr", "ggplot2", "readr", "dplyr", "tidyr", "patchwork", "devtools", "markdown"), lib="C:/ProgramData/R/Rlibs/R420")
+devtools::install_github("mjgreen/beginr", lib="C:/ProgramData/R/Rlibs/R420")
